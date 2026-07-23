9b2e06ad159c18bca3df0e54004722157be2d7e9
FernandoKraftSpaar <00343205@ufrgs.br>
2026-07-23 19:37:41 -0300

    Remove image element from FeaturedProduct
    Removed image element from FeaturedProduct component.

--- src/components/home/FeaturedProduct.tsx
+++ src/components/home/FeaturedProduct.tsx
@@ -9,11 +9,7 @@ export function FeaturedProduct() {
     <section className="bg-neutralbg py-24">
       <div className="container-cromax grid items-center gap-12 md:grid-cols-2">
         <div className="rounded-[2.5rem] bg-white p-4 shadow-xl">
-          <img
-            src={product.imageSrc}
-            alt={product.imageAlt}
-            className="aspect-[4/3] w-full rounded-[2rem] object-cover"
-          />
+          
         </div>
 
         <div>
